6b3202368b61c58e820fae083d0318ce0660035f
AlyAbdelrahman <aly.m.abdelrahman@gmail.com>
2022-06-02 01:34:13 +0200

    adding second table footer

--- src/components/TableFooter.js
+++ src/components/TableFooter.js
@@ -10,6 +10,12 @@ const styles = StyleSheet.create({
         display: 'flex',
         flexDirection: 'row'
     },
+    checkBoxContainerCellSecondFooter: {
+        display: 'flex',
+        flexWrap: 'nowrap',
+        flexDirection: 'row',
+        width: '90%',
+    },
     checkBoxContainerCell: {
         display: 'flex',
         flexWrap: 'nowrap',
@@ -72,7 +78,8 @@ const styles = StyleSheet.create({
         display: 'flex',
         justifyContent: 'center',
         alignContent: 'center',
-        width: '80%',
+        textAlign: 'center',
+        // width: '80%',
         paddingLeft: 2
     },
     smallcellDataContainer: {
@@ -83,13 +90,47 @@ const styles = StyleSheet.create({
         paddingLeft: 2
     },
     smallCellContainer: {
-        width: '6%',
+        width: '35%',
         borderWidth: 1,
         display: 'flex',
         flexDirection: 'row',
         alignContent: 'center',
         alignItems: 'center',
         paddingLeft: 2
+    },
+    smallUsedForCellContainer: {
+        width: '100%',
+        borderWidth: 1,
+        display: 'flex',
+        flexDirection: 'row',
+        alignContent: 'center',
+        alignItems: 'center',
+        paddingLeft: 2
+    },
+    stautsBar: {
+        width: '100%'
+    },
+    fullWidthCell: {
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'row'
+    },
+    fullWidthDataCell: {
+        width: '100%',
+        borderWidth: 1,
+        display: 'flex',
+        flexDirection: 'row',
+        alignContent: 'center',
+        alignItems: 'center',
+        paddingLeft: 2
+    },
+    usedFor: {
+        width: '100%'
+    },
+    secondFooter: {
+        display: 'flex',
+        flexDirection: 'row',
+        width: '100%'
     }
 })
 export default function TableFooter() {
@@ -128,19 +169,69 @@ export default function TableFooter() {
                     </View>
                 </View>
                 <View style={styles.smallCellContainer}>
-                <Text style={styles.smallText}>
+                    <Text style={styles.smallText}>
                         AFE
                     </Text>
                     <Text style={styles.smallText}>:</Text>
                     <View style={styles.smallcellDataContainer}>
-                        <Text style={styles.smallText}> 189 </Text>
+                        {/* <View></View>
+                        <View></View>
+                        <View></View>
+                        <View></View>
+                        <View></View>
+                        <View></View> */}
+
                     </View>
                 </View>
                 <View></View>
             </View>
-            <View></View>
-            <View></View>
-            <View></View>
+            <View style={styles.secondFooter}>
+                <View style={[styles.tableDetailsCellContainer, styles.stautsBar]}>
+                    <Text style={styles.smallText}>
+                        STATUS
+                    </Text>
+                    <Text style={styles.smallText}>:</Text>
+                    <View style={styles.checkBoxContainerCellSecondFooter}>
+                        <View style={styles.checkBoxContainer}>
+                            <View style={styles.checkBox} />
+                            <Text style={styles.smallText}>
+                                DIRECT CHARGE
+                            </Text>
+                        </View>
+                        <View style={styles.checkBoxContainer}>
+                            <View style={styles.checkBox} />
+                            <Text style={styles.smallText}>
+                                NON STOCK
+                            </Text>
+                        </View>
+                        <View style={styles.checkBoxContainer}>
+                            <View style={styles.checkBox} >
+                                <Image src="assets/images/check-mark-icon.jpg" style={styles.checkMark} />
+                            </View>
+                            <Text style={styles.smallText}>
+                                STOCK
+                            </Text>
+                        </View>
+                    </View>
+                </View>
+                <View style={[styles.fullWidthCell, styles.usedFor]}>
+                    <View style={styles.smallUsedForCellContainer}>
+                        <Text style={styles.smallText}>
+                            USED FOR
+                        </Text>
+                        <Text style={styles.smallText}>:</Text>
+                        <View style={styles.cellDataContainer}>
+                            <Text style={styles.smallText}> 189 test budget</Text>
+                        </View>
+                    </View>
+                </View>
+            </View>
+            <View style={styles.thirdFooter}>
+                <View></View>
+                <View></View>
+                <View></View>
+                <View></View>
+            </View>
 
         </View>
     )
